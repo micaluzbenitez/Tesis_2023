26091848bdb514244a7e8d15640640174df97f37
micaluzbenitez <benitezmicaelaluz@gmail.com>
2023-05-04 11:40:12 -0300

    Car move to click position

--- tesis_2023/Assets/Scripts/CarNavMesh.cs
+++ tesis_2023/Assets/Scripts/CarNavMesh.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+// Player controller
+
 public class CarNavMesh : MonoBehaviour
 {
     [SerializeField] private Transform goal;
@@ -16,6 +18,22 @@ public class CarNavMesh : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetMouseButton(0)) GoToGoal();
+        if (Input.GetMouseButton(1)) GoToClick();
+    }
+
+    private void GoToGoal()
+    {
         agent.destination = goal.position;
+    }
+
+    private void GoToClick()
+    {
+        Ray movePosition = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(movePosition, out var hitInfo))
+        {
+            agent.SetDestination(hitInfo.point);
+        }
     }
 }
