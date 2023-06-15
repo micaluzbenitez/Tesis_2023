a6809773daccff7060c0abc6ae4a70173de23679
pichi02 <lucas.pich02@gmail.com>
2023-06-14 23:22:15 -0300

    Clean CarLifeBehaviour script

--- tesis_2023/Assets/Scripts/Entities/CarLifeBehaviour.cs
+++ tesis_2023/Assets/Scripts/Entities/CarLifeBehaviour.cs
@@ -17,67 +17,69 @@ namespace Entities
         {
             currentHealth = maxHealth;
             rb = GetComponent<Rigidbody>();
-
-
-            previousPosition = transform.position;
-            previousTime = Time.time;
+            InitVelocityData();
         }
         private void Update()
         {
-
-            Vector3 currentPosition = transform.position;
-            float currentTime = Time.time;
-            Vector3 displacement = currentPosition - previousPosition;
-            velocity = displacement / (currentTime - previousTime);
-
-            previousPosition = currentPosition;
-            previousTime = currentTime;
-            if (velocity != Vector3.zero)
-            {
-                previousVelocity = velocity;
-            }
-
-            if (gameObject.name == "Player")
-            {
-                Debug.Log((int)velocity.magnitude);
-
-            }
-
-        }
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (collision.gameObject.CompareTag("Car"))
-            {
-
-                Vector3 collisionNormal = collision.contacts[0].normal;
-
-                // Calcular el producto punto entre la dirección forward del objeto actual y la normal de la colisión
-                float dotProduct = Vector3.Dot(transform.forward, collisionNormal);
-
-                // Si el resultado del producto punto es mayor a 0.9, el objeto está de frente al colisionar
-                if (dotProduct < -0.8f || dotProduct > 0.8f)
-                {
-                    Debug.Log("de frente");
-                    CarLifeBehaviour otherCarLife = collision.gameObject.GetComponent<CarLifeBehaviour>();
-                    if (otherCarLife != null)
-                    {
-
-                        otherCarLife.TakeDamage(previousVelocity.magnitude / 2f);
-                    }
-                }
-
-            }
+            UpdateVelocityData();
         }
 
-        public void TakeDamage(float damage)
+        private void TakeDamage(float damage)
         {
             currentHealth -= (int)damage;
             Debug.Log(gameObject.name + " life: " + currentHealth);
 
             if (currentHealth <= 0)
             {
+                Debug.Log(gameObject.name + " is dead");
+            }
+        }
 
-                Destroy(gameObject);
+        private void InitVelocityData()
+        {
+            previousPosition = transform.position;
+            previousTime = 0f;
+        }
+        private void UpdateVelocityData()
+        {
+            Vector3 currentPosition = transform.position;
+            float currentTime = Time.time;
+            Vector3 displacement = currentPosition - previousPosition;
+            velocity = displacement / (currentTime - previousTime);
+            previousPosition = currentPosition;
+            previousTime = currentTime;
+
+            if (velocity != Vector3.zero)
+            {
+                previousVelocity = velocity;
+            }
+        }
+        private void ToDamageOpponent(Collision collision)
+        {
+            CarLifeBehaviour otherCarLife = collision.gameObject.GetComponent<CarLifeBehaviour>();
+
+            if (otherCarLife != null)
+            {
+                otherCarLife.TakeDamage(previousVelocity.magnitude / 2f);
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Car"))
+            {
+                Vector3 collisionNormal = collision.contacts[0].normal;
+                float dotProduct = Vector3.Dot(transform.forward, collisionNormal);
+                float allowedAngle = 0.8f;
+
+
+                if (dotProduct < -allowedAngle || dotProduct > allowedAngle)
+                {
+                    Debug.Log("de frente");
+                    ToDamageOpponent(collision);
+
+                }
+
             }
         }
     }
