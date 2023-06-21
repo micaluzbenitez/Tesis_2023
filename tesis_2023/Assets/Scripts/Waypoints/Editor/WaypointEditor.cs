f59d3130bda82b5e52244ab07524e0943f02551f
micaluzbenitez <benitezmicaelaluz@gmail.com>
2023-06-21 16:00:24 -0300

    Fix waypoints

--- tesis_2023/Assets/Scripts/Waypoints/Editor/WaypointEditor.cs
+++ tesis_2023/Assets/Scripts/Waypoints/Editor/WaypointEditor.cs
@@ -19,7 +19,7 @@ namespace Waypoints.Editor
                 Gizmos.color = Color.yellow * 0.5f;
             }
 
-            Gizmos.DrawSphere(waypoint.transform.position, 1f);
+            Gizmos.DrawSphere(waypoint.transform.position, 2f);
 
             Gizmos.color = Color.blue;
             Gizmos.DrawLine(waypoint.transform.position + (waypoint.transform.right * waypoint.width / 2f),
