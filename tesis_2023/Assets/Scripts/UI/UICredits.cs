3b414e86a5513156383900f10d85dfc69001ee54
micaluzbenitez <benitezmicaelaluz@gmail.com>
2023-11-30 00:04:21 -0300

    Implemented credits

--- tesis_2023/Assets/Scripts/UI/UICredits.cs
+++ tesis_2023/Assets/Scripts/UI/UICredits.cs
@@ -1,9 +1,16 @@
+using Managers;
 using UnityEngine;
 
 namespace UI
 {
     public class UICredits : MonoBehaviour
     {
-        
+        [Header("Scenes")]
+        [SerializeField] private string mainMenuSceneName = "";
+
+        public void LoadMainMenuScene()
+        {
+            LoaderManager.Get().LoadScene(mainMenuSceneName);
+        }
     }
 }
