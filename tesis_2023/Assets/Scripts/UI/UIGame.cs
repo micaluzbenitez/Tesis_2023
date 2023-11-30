9d105c608b6010051f09a5a0e959a89a305d7828
micaluzbenitez <benitezmicaelaluz@gmail.com>
2023-11-30 00:52:25 -0300

    Implement pause

--- tesis_2023/Assets/Scripts/UI/UIGame.cs
+++ tesis_2023/Assets/Scripts/UI/UIGame.cs
@@ -1,9 +1,38 @@
+using Managers;
 using UnityEngine;
 
 namespace UI
 {
     public class UIGame : MonoBehaviour
     {
-        
+        [Header("Scenes")]
+        [SerializeField] private string gameSceneName = "";
+        [SerializeField] private string mainMenuSceneName = "";
+
+        [Header("Pause")]
+        [SerializeField] private GameObject optionsPanel = null;
+
+        public void LoadGameScene()
+        {
+            LoaderManager.Get().LoadScene(gameSceneName);
+        }
+
+        public void LoadMainMenuScene()
+        {
+            Time.timeScale = 1;
+            LoaderManager.Get().LoadScene(mainMenuSceneName);
+        }
+
+        public void Pause()
+        {
+            optionsPanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        public void Resume()
+        {
+            optionsPanel.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
