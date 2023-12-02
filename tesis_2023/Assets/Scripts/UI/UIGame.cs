14117b86abc7b92bed0438f4603b7baf5284d521
micaluzbenitez <benitezmicaelaluz@gmail.com>
2023-12-01 22:44:39 -0300

    Add ESC to pause

--- tesis_2023/Assets/Scripts/UI/UIGame.cs
+++ tesis_2023/Assets/Scripts/UI/UIGame.cs
@@ -12,26 +12,41 @@ namespace UI
         [Header("Pause")]
         [SerializeField] private GameObject optionsPanel = null;
 
+        private bool pauseGame = false;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (!pauseGame) Pause();
+                else Resume();
+            }
+        }
+
         public void LoadGameScene()
         {
+            pauseGame = false;
             Time.timeScale = 1;
             LoaderManager.Get().LoadScene(gameSceneName);
         }
 
         public void LoadMainMenuScene()
         {
+            pauseGame = false;
             Time.timeScale = 1;
             LoaderManager.Get().LoadScene(mainMenuSceneName);
         }
 
         public void Pause()
         {
+            pauseGame = true;
             optionsPanel.SetActive(true);
             Time.timeScale = 0;
         }
 
         public void Resume()
         {
+            pauseGame = false;
             optionsPanel.SetActive(false);
             Time.timeScale = 1;
         }
